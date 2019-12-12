ce3e4445007de4ebdbc5d4ee565d66c4dbb0f458
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-12 09:21:19 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,7 +6,7 @@ class Backer
     @backed_projects=[]
   end
   def back_project(project)
-    @backed_projects<<projects
+    @backed_projects<<project
     project.add_backer(self)
   end
   def backed_projects
